39a47906ca8fbe1360d37244960e685c4b99a595
Rugved Patel <patelrugved7@gmail.com>
2022-03-23 06:10:36 -0700

    initial commit v1

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{display:"flex", flexDirection:"row", justifyContent:"center", alignItems:"center"}}> 
+      <h1>
+      THIS IS NEW PRAGET X WEBSITE
+
+      </h1>
+    </div>
+  );
+}
+
+export default App;
