2e40ebb74c5586a3fe090b777f115aae406ffc9f
Rugved Patel <patelrugved7@gmail.com>
2022-04-19 16:28:26 +0530

    mapadded

--- src/Components/AcrossTheWorld/AcrossTheWorld.tsx
+++ src/Components/AcrossTheWorld/AcrossTheWorld.tsx
@@ -1,49 +1,74 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '100%',
-  height: '650px'
+   width: '100%',
+   height: '650px'
 };
-
 const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+   lat:22,
+   lng:22
+}
+const Places = [{
+   lat: 43.7228,
+   lng: 10.4017
+}, {
+   lat: 25.27,
+   lng: -133.7751
+}, {
+   lat: 37.092,
+   lng: 95.523
+}, {
+   lat: 56.745,
+   lng: 106.523
+}]
 
 function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyAjAnhj49a-8wxlQLZ2ZWNwvsaF5le7n5c"
-  })
+   const { isLoaded } = useJsApiLoader({
+      id: 'google-map-script',
+      googleMapsApiKey: "AIzaSyBYEu3SQcQzvWTCJ5FPiytYHCSCg9T6ETU"
+   })
 
-  const [map, setMap] = React.useState(null)
+   const [map, setMap] = React.useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+   const onLoad = React.useCallback(function callback(map) {
+      var latlngbounds = new google.maps.LatLngBounds();
+      for (var i = 0; i < Places.length; i++) {
+        latlngbounds.extend(Places[i]);
+      }
+      map.fitBounds(latlngbounds);
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+      setMap(map)
+   }, [])
+   const onUnmount = React.useCallback(function callback(map) {
+      setMap(null)
+    }, [])
 
-  return isLoaded ? (
-     <div className='col-12' style={{width:"100%",height:"100%"}}>
-      <h3 className='text-center'>Our Foot Prints</h3>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-         </div>
-  ) : <></>
+console.log(map)
+   return isLoaded ? (
+      <div className='col-12' style={{ width: "100%", height: "100%" }}>
+         <h3 className='text-center'>Our Foot Prints</h3>
+         <GoogleMap
+            zoom={0}
+            mapContainerStyle={containerStyle}
+            onLoad={onLoad}
+            
+         >
+            { /* Child components, such as markers, info windows, etc. */}
+            {
+               Places && Places.map((i) => (
+                  <Marker position={i}>
+                     <InfoWindow >
+                        <span>Pragetxian</span>
+                     </InfoWindow>
+                  </Marker>
+
+               ))
+            }
+
+         </GoogleMap>
+      </div>
+   ) : <></>
 }
 
 export default React.memo(MyComponent)
